f5a680984d8be3af6e8fc226a82ec07b18f11ec1
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-11-06 08:52:35 -0300

    Criado funcionalidades LINQ, WHere, OrderBy, Revert

--- ExemplosLinq/Program.cs
+++ ExemplosLinq/Program.cs
@@ -11,38 +11,35 @@ namespace ExemplosLinq
             // FONTE DE DADOS
             var listaProdutos = new List<Produto>()
             {
-                new Produto{Id = 9, CategoriaId = 2, Nome = "Geladeira", Status = true, Valor = 10},
-                new Produto{Id = 2, CategoriaId = 3, Nome = "Short", Status = true, Valor = 1},
-                new Produto{Id = 4, CategoriaId = 1, Nome = "Maquina de lavar", Status = false, Valor = 32},
-                new Produto{Id = 3, CategoriaId = 1, Nome = "Video Game", Status = true, Valor = 99},
-                new Produto{Id = 6, CategoriaId = 2, Nome = "Arroz", Status = true, Valor = 55},
-                new Produto{Id = 8, CategoriaId = 1, Nome = "TV", Status = true, Valor = 45},
-                new Produto{Id = 1, CategoriaId = 3, Nome = "Camiseta", Status = true, Valor = 100},
-                new Produto{Id = 5, CategoriaId = 1, Nome = "Microondas", Status = true, Valor = 90},
-                new Produto{Id = 7, CategoriaId = 2, Nome = "Feijão", Status = true, Valor = 12},
+                new Produto {Id = 9, CategoriaId = 2, Nome = "Geladeira", Status = true, Valor = 10},
+                new Produto {Id = 2, CategoriaId = 3, Nome = "Short", Status = true, Valor = 1},
+                new Produto {Id = 4, CategoriaId = 1, Nome = "Maquina de lavar", Status = false, Valor = 32},
+                new Produto {Id = 3, CategoriaId = 1, Nome = "Video Game", Status = true, Valor = 99},
+                new Produto {Id = 6, CategoriaId = 2, Nome = "Arroz", Status = true, Valor = 55},
+                new Produto {Id = 8, CategoriaId = 1, Nome = "TV", Status = true, Valor = 45},
+                new Produto {Id = 1, CategoriaId = 3, Nome = "Camiseta", Status = true, Valor = 100},
+                new Produto {Id = 5, CategoriaId = 1, Nome = "Microondas", Status = true, Valor = 90},
+                new Produto {Id = 7, CategoriaId = 2, Nome = "Feijão", Status = true, Valor = 12},
             };
 
-            var listaCategorias = new List<Categoria>()
-            {
-                new Categoria{Id = 1, Status = true, Nome = "Eletronicos"},
-                new Categoria{Id = 2, Status = true, Nome = "Alimentos"},
-                new Categoria{Id = 3, Status = true, Nome = "Vestuario"},
-            };
+            // where 
+            var resultado = listaProdutos.Where(prod => FiltraProdutoPorValor(prod));
 
-            var resultado = from prod in listaProdutos
-                            join cat in listaCategorias
-                            on prod.CategoriaId equals cat.Id
-                            select new
-                            {
-                                Produto = prod,
-                                Categoria = cat
-                            };
+            // order by orderby descending
+            resultado = resultado.OrderByDescending(prod => prod.Id);
 
-            //EXECUTAR A CONSULTA
+            // revert
+            resultado = resultado.Reverse();
+
             foreach (var result in resultado)
             {
-                Console.WriteLine($"Nome: {result.Produto.Nome} | Categoria: {result.Categoria.Nome}");
+                Console.WriteLine($"ID: {result.Id} | Nome {result.Nome}");
             }
+        }
+
+        static bool FiltraProdutoPorValor(Produto produto)
+        {
+            return produto.Valor > 50;
         }
     }
 
